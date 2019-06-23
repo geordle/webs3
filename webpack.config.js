62f65c1f48fcbed0cef0ed1b44fed7d7b83c7181
Ylja07 <yson@avans.nl>
2019-06-23 14:00:59 +0200

    naam en fav

--- webpack.config.js
+++ webpack.config.js
@@ -15,61 +15,64 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components|build|dist)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components|build|dist)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
                 },
             },
-            {
-                test: /\.global.css$/,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.min.css$/,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /(?<!global|min)\.css$/,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.html$/,
-                use: 'raw-loader',
-            },
-            //only for bootstrap
-            {
-                test: /\.(scss)$/,
-                use: [{
-                    loader: 'style-loader', // inject CSS to page
-                }, {
-                    loader: 'css-loader', // translates CSS into CommonJS modules
-                }, {
-                    loader: 'postcss-loader', // Run post css actions
-                    options: {
-                        plugins: function () { // post css plugins, can be exported to postcss.config.js
-                            return [
-                                require('precss'),
-                                require('autoprefixer')
-                            ];
-                        }
+        },
+        {
+            test: /\.global.css$/,
+            use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.min.css$/,
+            use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+            test: /(?<!global|min)\.css$/,
+            use: 'raw-loader',
+        },
+        {
+            test: /\.html$/,
+            use: 'raw-loader',
+        },
+        //only for bootstrap
+        {
+            test: /\.(scss)$/,
+            use: [{
+                loader: 'style-loader', // inject CSS to page
+            }, {
+                loader: 'css-loader', // translates CSS into CommonJS modules
+            }, {
+                loader: 'postcss-loader', // Run post css actions
+                options: {
+                    plugins: function () { // post css plugins, can be exported to postcss.config.js
+                        return [
+                            require('precss'),
+                            require('autoprefixer')
+                        ];
                     }
-                }, {
-                    loader: 'sass-loader' // compiles Sass to CSS
-                }]
-            },
+                }
+            }, {
+                loader: 'sass-loader' // compiles Sass to CSS
+            }]
+        },
         ],
     },
-    plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin({
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Monster Zoo',
+        favicon : 'icon.png'
+    }), new MiniCssExtractPlugin({
         filename: 'style.css',
     })],
     stats: {
